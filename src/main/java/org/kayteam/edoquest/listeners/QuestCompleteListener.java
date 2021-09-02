26ca9a3297780f07fa77fa49315590859edba0dc
segu23 <segundoschmidt@gmail.com>
2021-09-02 13:51:41 -0300

    Update

--- src/main/java/org/kayteam/edoquest/listeners/QuestCompleteListener.java
+++ src/main/java/org/kayteam/edoquest/listeners/QuestCompleteListener.java
@@ -1,10 +1,13 @@
 package org.kayteam.edoquest.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.kayteam.edoquest.EdoQuest;
 import org.kayteam.edoquest.events.QuestCompleteEvent;
+
+import java.util.logging.Level;
 
 public class QuestCompleteListener implements Listener {
 
@@ -16,8 +19,12 @@ public class QuestCompleteListener implements Listener {
 
     @EventHandler
     public void onQuestComplete(QuestCompleteEvent event){
-        Bukkit.getLogger().info("PRESTIGE COMPLETE");
-        Bukkit.getLogger().info(event.getPlayer().toString());
-        Bukkit.getLogger().info(event.getPrestige().toString());
+        String group = event.getPrestige().getPrestigeRank();
+        try{
+            EdoQuest.getPermissions().playerAddGroup(event.getPlayer(), group);
+            Bukkit.broadcastMessage("El jugador "+event.getPlayer().getName()+" subió al prestigio "+event.getPrestige().getDisplayName());
+        }catch (Exception e){
+            Bukkit.getLogger().log(Level.SEVERE, ChatColor.translateAlternateColorCodes('&', "&c[EdoQuest] An error has ocurred trying to give &f"+group+" &cto &f"+event.getPlayer().getName()));
+        }
     }
 }
